7d3930fa9d92c396b1cb6e275eb6e5ce8287f966
Bhashini123868 <bhashimunasinghe286@gmail.com>
2025-06-29 22:24:19 +0530

    dto classess

---
+++ src/main/java/edu/icet/mos/dto/Item.java
@@ -0,0 +1,28 @@
+package edu.icet.mos.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Item {
+    private Integer id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotNull(message = "Price is required")
+    @Min(value = 1, message = "Price must be greater than zero")
+    private Double price;
+
+    @NotBlank(message = "Image URL is required")
+    private String image;
+}
---
+++ src/main/java/edu/icet/mos/dto/Order.java
@@ -0,0 +1,32 @@
+package edu.icet.mos.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Order {
+    private Integer orderID;
+
+    @NotBlank(message = "Customer ID is required")
+    private String cusID;
+
+    @NotNull(message = "Total is required")
+    @Min(value = 1, message = "Total must be greater than zero")
+    private Double total;
+
+    @NotBlank(message = "Date is required")
+    private String date;
+
+    @NotNull(message = "Order details list cannot be null")
+    private List<OrderDetailsEntity> orderDetail;
+}
